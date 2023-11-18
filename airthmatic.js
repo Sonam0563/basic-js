e7370da7c621ff32432f1365cb2dbca613fd4ee1
first <msonam189@gmail.com>
2023-11-18 10:35:05 -0800

    add another program of javascript

---
+++ airthmatic.js
@@ -0,0 +1,11 @@
+console.log("all airthmatic operation")
+let a=4
+let b=6
+console.log("addition",a+b)
+console.log("subtraction",a-b)
+console.log("multiplication",a*b)
+console.log("division",a/b)
+console.log("remainder",a%b)
+console.log("increement",a++)
+console.log("decrement",a--)
+console.log("result after decrement",a)
