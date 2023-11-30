056981988417f647dcbc13ef71dabbe53e764f60
first <msonam189@gmail.com>
2023-11-29 18:29:19 -0800

    new file

--- basic.js
+++ basic.js
@@ -1,2 +1,26 @@
-console.log("hello world")
-console.log("live")
+/*console.log("hello world")
+console.log("live")*/
+//question (1)on switch
+/*let age=prompt("what is your age")
+switch (age){
+        case '12':
+         console.log("your age is 12")
+         break
+        case '13':
+         console.log("your age is 13")
+         break
+        case '14':
+         console.log("your age is 14")
+         break
+        case '15':
+         console.log("your age is 15")
+         break
+        case '16':
+         console.log("your age is 16")
+        default:
+         console.log("your age is not special")
+}*/
+//question (2) based on ternary
+let age=19
+let a = age>18?"you can drive":"you cannot drive"
+console.log(a)
---
+++ mapfilter_reduce.js
@@ -0,0 +1,5 @@
+//array reduce method
+let arr=[1,2,3,5,8]
+arr.reduce((h1,h2)=>{
+    return h1+h2
+})
