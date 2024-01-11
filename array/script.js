d1534b020514a38821021017b90dcc79503461f2
first <msonam189@gmail.com>
2024-01-11 01:01:32 -0800

    file added

---
+++ array/script.js
@@ -0,0 +1,3 @@
+let arr=[1,2,3,4,5,7]
+console.log(arr);
+console.log(arr.length)
---
+++ promt_alert_confirm.js
@@ -0,0 +1,4 @@
+alert("enter the value of a")
+let a = prompt("enter a here","234")
+let write = confirm("do you want to write it on page")
+document.write(a)
